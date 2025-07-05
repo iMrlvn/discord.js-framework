9a004a720a8496bb5c58d79865433403e8d896ae
iMrlvn <mrlvndev@gmail.com>
2025-07-05 18:23:25 +0700

    update index.ts

--- lib/framework/index.ts
+++ lib/framework/index.ts
@@ -9,18 +9,26 @@ export interface FrameworkOptions{
 };
 
 export class Framework {
+    private _client: Client;
+    private commandsPath: string;
+    private eventsPath: string;
+    private devGuilds: string[];
+    private devUsers: string[];
+    
     constructor(options: FrameworkOptions) {
         if (!options) return console.log("Framework", new Error("Need options!"));
         
         this._client = options.client;
         this.commandsPath = options.commandsPath;
         this.eventsPath = options.eventsPath;
-        this.devGuilds = options.devGuilds;
-        this.devUsers = options.devUsers;
+        this.devGuilds = options.devGuilds ?? [];
+        this.devUsers = options.devUsers ?? [];
         
         this.loadEverything();
     }
-    async loadEverything(): Promise<void> {
+    
+    private async loadEverything() {
         // next
+        return this;
     }
 }
