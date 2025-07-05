6d7927ed75b80fba42c934682c6585e415af8980
iMrlvn <mrlvndev@gmail.com>
2025-07-06 03:28:27 +0700

    update Framework.ts

--- lib/plugins/Framework.ts
+++ lib/plugins/Framework.ts
@@ -2,6 +2,8 @@ import EventEmitter from "node:events";
 import { Client } from "discord.js";
 import { FrameworkOptions, Logger } from "../types";
 import { readdirSync } from "node:fs";
+import { default as commands } from "../handlers/command";
+import { default as events } from "../handlers/event";
 
 export class Framework extends EventEmitter {
     private _client: Client | null;
@@ -22,9 +24,7 @@ export class Framework extends EventEmitter {
         this.logger = options.logger || { handlers: true };
     }
     async loadEverything() {
-        const commands = await import("../handlers/command");
         commands(this.commandsPath, this._client, this);
-        const events = await import("../handlers/event");
         events(this.eventsPath, this._client, this);
     }
 }
