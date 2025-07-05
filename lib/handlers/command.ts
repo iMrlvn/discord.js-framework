5332e8982f75aac28a3f769b9d7e1784ffff9d10
iMrlvn <mrlvndev@gmail.com>
2025-07-06 02:05:52 +0700

    update command.ts

--- lib/handlers/command.ts
+++ lib/handlers/command.ts
@@ -4,6 +4,6 @@ import { FrameworkOptions } from "../types";
 
 export default async function(commandsPath: string, client: Client, options: FrameworkOptions={}): Promise<void> {
     const amount: object[] = [];
-    if (options.logger!.handlers) console.log("[Handlers]", folderPath, "opened...");
+    if (options.logger!.handlers) console.log("[Handlers]", commandsPath, "opened...");
     console.log("[Handlers]", commandsPath, "files loaded", `${amount.length} commands`);
 }
