3b1607fa51e4dfb16cf0c715fd46f1b9b2fabfd2
iMrlvn <mrlvndev@gmail.com>
2025-07-10 19:21:32 +0700

    update index.js

--- test/index.js
+++ test/index.js
@@ -5,7 +5,7 @@ const { readdirSync } = require("node:fs");
 readdirSync("./lib/plugins")
     .forEach(file => {
         try {
-            const Plugin = require(`../plugins/${file}`);
+            const Plugin = require(`../plugins/${file.split(".")[0]}`);
             console.log(new Plugin());
         } catch(error) {
             console.log(error);
