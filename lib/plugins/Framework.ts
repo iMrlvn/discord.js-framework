7cc7997939006391527dbfb37c7e00443357399b
iMrlvn <mrlvndev@gmail.com>
2025-07-10 18:39:22 +0700

    update Framework.ts

--- lib/plugins/Framework.ts
+++ lib/plugins/Framework.ts
@@ -6,25 +6,28 @@ import { default as commands } from "../handlers/command";
 import { default as events } from "../handlers/event";
 
 export class Framework extends EventEmitter {
-    private client: Client | null;
-    private commandsPath: string;
-    private eventsPath: string;
-    private devGuilds: string[];
-    private devUsers: string[];
-    public logger: Logger;
-    
-    constructor(options: FrameworkOptions = {}) {
+    public client: Client | null = null;
+    public commandsPath: string = "./commands";
+    public eventsPath: string = "./events";
+    public devGuilds: string[] = [];
+    public devUsers: string[] = [];
+    public logger: Logger = { handlers: true };
+
+    public constructor(options: FrameworkOptions) {
         super();
         
-        this.client = options.client || null;
-        this.commandsPath = options.commandsPath || "./commands";
-        this.eventsPath = options.eventsPath || "./events";
-        this.devGuilds = options.devGuilds || [];
-        this.devUsers = options.devUsers || [];
-        this.logger = options.logger || { handlers: true };
+        this.client = options.client;
+        this.commandsPath = options.commandsPath;
+        this.eventsPath = options.eventsPath;
+        this.devGuilds = options.devGuilds;
+        this.devUsers = options.devUsers;
+        this.logger = options.logger;
+
+        this.loadEverything();
     }
-    async loadEverything() {
+    private async loadEverything() {
         commands(this.commandsPath, this as Framework);
         events(this.eventsPath, this as Framework);
+        return this as Framework;
     }
 }
